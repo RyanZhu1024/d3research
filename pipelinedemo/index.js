04d3f3a3c8b47b7fa1192315af336f029d3bb603
Ryan Zhu <rzhu@snaplogic.com>
2020-02-07 14:08:18 -0800

    add data

--- pipelinedemo/index.js
+++ pipelinedemo/index.js
@@ -2,34 +2,70 @@ import shortid from "shortid";
 import * as d3 from "d3";
 import "./styles.css";
 const TYPE = {
-  READ: { name: "read", color: '#16af11' },
-  PARSE: { name: "parse", color: '#FECD01' },
-  TRANSFORM: { name: "transform", color: '#4c4cfc' },
-  FLOW: { name: "flow", color: '#06c8c9' },
-  FORMAT: { name: "format", color: '#ffdbac' },
-  WRITE: { name: "write", color: '#d581d9' }
+  READ: {
+    name: "read", color: '#16af11', data: {
+      shape: 'polygon',
+      shapeKey: 'points',
+      shapeValue: '13.7376816 -1.13686838e-13 62 -1.10044352e-13 48.5884438 64 8.81072992e-13 64'
+    }
+  },
+  PARSE: {
+    name: "parse", color: '#FECD01', data: {
+      shape: 'polygon',
+      shapeKey: 'points',
+      shapeValue: '67.1111111 1.10134124e-13 99.5534668 64 34.9741211 64'
+    }
+  },
+  TRANSFORM: {
+    name: "transform", color: '#4c4cfc', data: {
+      shape: 'polygon',
+      shapeKey: 'points',
+      shapeValue: '0.5 -2.5243549e-29 62.5 1.13686838e-13 62.5 64 0.5 64'
+    }
+  },
+  FLOW: {
+    name: "flow", color: '#06c8c9', data: {
+      shape: 'polygon',
+      shapeKey: 'points',
+      shapeValue: '10.1923882 9.372583 55.0936688 9.4713024 55.4472222 54.627417 10.2825028 54.2824683'
+    }
+  },
+  FORMAT: {
+    name: "format", color: '#ffdbac', data: {
+      shape: 'path',
+      shapeKey: 'd',
+      shapeValue: 'M41,0 L103.053467,0 C103.053467,31.7568359 103.053467,47.6352539 103.053467,47.6352539 C68.9863281,42.627417 81.5400391,73.1982422 41,60.9418945 C41,60.9418945 41,40.6279297 41,0 Z'
+    }
+  },
+  WRITE: {
+    name: "write", color: '#d581d9', data: {
+      shape: 'path',
+      shapeKey: 'd',
+      shapeValue: 'M102.465187,0 C102.465187,0 54.0915693,0 54.0915693,0 C36.5241674,17.0163039 35.6392161,47.2629566 53.2066179,64 C53.2066179,64 102.465187,63.8647624 102.465187,63.8647624 C87.2510825,47.1763573 87.2510825,16.6337554 102.465187,0 Z'
+    }
+  }
 };
 
 const pipelineNodes = [
   {
     id: shortid.generate(),
     type: TYPE.READ,
-    name: "File Reader"
+    name: "File Reader",
   },
   {
     id: shortid.generate(),
     type: TYPE.PARSE,
-    name: "CSV Parser"
+    name: "CSV Parser",
   },
   {
     id: shortid.generate(),
     type: TYPE.TRANSFORM,
-    name: "Mapper"
+    name: "Mapper",
   },
   {
     id: shortid.generate(),
     type: TYPE.FLOW,
-    name: "Copy"
+    name: "Copy",
   },
   {
     id: shortid.generate(),
@@ -126,10 +162,11 @@ const linksGroup = root
 const nodesGroup = root
   .append("g")
   .attr("class", "nodesGroup")
-  .selectAll("circle")
+  .selectAll(function (d) { return d.type.shape })
   .data(pipelineNodes)
   .enter()
-  .append('circle')
+  .append(function (d) { return d.type.shape })
+  .attr(function (d) { return d.type.shapeKey }, function (d) { return d.type.shapeValue })
   .attr("r", 10)
   .attr('fill', function (d) { return d.type.color })
   .call(
@@ -172,5 +209,5 @@ function tick() {
     .attr("y2", function (d) {
       return d.target.y;
     });
-  nodesGroup.attr('cx', function (d) { return d.x }).attr('cy', function (d) { return d.y })
+  nodesGroup.attr('transform', function (d) { return `translate(${d.x}, ${d.y})` })
 };
